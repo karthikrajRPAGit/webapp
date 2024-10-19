14f1f8b673d5363dc8420ec10f6b5a41e8817723
Karthikraj <karthikrajrpa@gmail.com>
2024-10-19 09:09:26 +0530

    Changes done to package the code

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="webapp",
+    version="0.0.1",
+    package_dir={"":"app"},
+    packages=find_packages(where="app"),
+    install_requires=["flask","requests"],
+    python_requires=">=3.6",
+)
