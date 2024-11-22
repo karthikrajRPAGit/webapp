d2e19ec90d654583a5e182e4bcdf1cdcf6d98abd
Karthikraj <karthikrajrpa@gmail.com>
2024-11-22 23:37:01 +0530

    updated package version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="webapp",
-    version="0.0.1",
+    version="0.0.2",
     package_dir={"":"app"},
     packages=find_packages(where="app"),
     install_requires=["flask","requests"],
